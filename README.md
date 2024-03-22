6218d67535490499a8fd5e0ca2dec51aacb2be6a
daniel <danny.killen@gmail.com>
2024-03-22 15:26:23 +0000

    Updated README and documentation in code

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # roulette-bot
 This is a simple bot to play an online game of roulette at https://www.247roulette.org/; it is not intended to win but demonstrate key coding concepts needed to automate a simple in-browser game.
 
-Through this project I learnt about OpenCV, Pillow, PyTesseract and PyWin
+## Built With
+- Pillow
+- OpenCV
+- PyTesseract
+- PyWin
+
+## Use
+The script is currently configured to run with the web game opened in a Brave browser window measuring 1720x1440 pixels.
+
+Prior to running for the first time the constants X_PAD and Y_PAD should be updated for your configuration to represent the top left corner of the game area. This should allow the script to function with alternate setups however it has not been tested.
+
+## Function
+The script runs for twenty spins of each time selecting a random number to bet upon.
+
+After each spin the even/odd and black/red events are tracked, once one has happened three times in a row a bet is placed for the opposite event.
+
--- bot_code.py
+++ bot_code.py
@@ -14,18 +14,27 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 X_PAD = 1909
 Y_PAD = 194
 
-def mousePos(cord):
+def mouse_pos(cord):
+    """
+    Takes (x, y) coordinates as a tuple, applies the correction for the game area origin and moves the mouse position to these coordinates.
+    """
     loc=(X_PAD + cord[0], Y_PAD + cord[1])
     win32api.SetCursorPos(loc)
     time.sleep(.1)
 
-def leftClick():
+def left_click():
+    """
+    Sends an event representing a click of the left mouse button.
+    """
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     time.sleep(.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
     time.sleep(.1)  
 
 def place_bet(cord):
+    """
+    Takes (x,y) coordinates as a tuple, applies the correction for the game area origin, moves the mouse to this location and left clicks
+    """
     loc=(X_PAD + cord[0], Y_PAD + cord[1])
     win32api.SetCursorPos(loc)
     time.sleep(.1)
@@ -34,6 +43,9 @@ def place_bet(cord):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)    
 	
 def get_cords():
+    """
+    Returns the x,y coordinates of the cursor adjusted for the game area origin
+    """
     x,y = win32api.GetCursorPos()
     x = x - X_PAD
     y = y - Y_PAD
@@ -41,22 +53,30 @@ def get_cords():
     return clicked_cord
 
 def bet_clear():
-    mousePos(bet_cord['clear'])
-    leftClick()
+    """
+    Clears all bets from the roulette board
+    """
+    mouse_pos(bet_cord['clear'])
+    left_click()
 
 def spin():
-    mousePos((1193, 952))
-    leftClick()
+    """ 
+    Clicks the spin button to start the roulette wheel spinning and clicks through the subsequent events to speed the process up
+    """
+    mouse_pos((1193, 952))
+    left_click()
     time.sleep(.1)
-    leftClick()
+    left_click()
     time.sleep(.1)
-    leftClick()
+    left_click()
     time.sleep(1)
-    leftClick()
+    left_click()
     time.sleep(1)
 
 def get_result(num_results, even_tracker, red_tracker):
-
+    """
+    Takes three global variables which track results between spins and updates them based on the last result
+    """
     red = 0
     even = 0
 
@@ -64,7 +84,7 @@ def get_result(num_results, even_tracker, red_tracker):
     box = (X_PAD + 1246, Y_PAD + 344, X_PAD + 1281, Y_PAD + 382)
     img = ImageGrab.grab(box)
 
-    # Get the number
+    # "Read" the image
     arr = np.array(img) # OpenCV and PyTesseract need array input
     gry = cv2.cvtColor(arr, cv2.COLOR_BGR2GRAY)
     txt = image_to_string(gry, config="--psm 7")
@@ -89,13 +109,12 @@ def get_result(num_results, even_tracker, red_tracker):
     num_results.insert(0, int(txt))
     if len(num_results) > 20:
         num_results.pop()
-    
     even_tracker += even
     red_tracker += red
 
     return num_results, even_tracker, red_tracker
 
-bet_cord = {
+bet_cord = { # This dictionary stores the (x, y) coordinates of the main locations on the roulette board relative to the game area origin
 00 : (175, 416),
 0 : (180, 551),
 1 : (256, 576),
@@ -150,38 +169,20 @@ bet_cord = {
 }
 
 def main():
-    # Bot Logic
+    """
+    Runs the main loop of the bot; spinning 20 times with random numbers and betting when even/odd and red/black events are likely
+    """
     num_results = []
     even_tracker = 0 # This tracks the current likelihood of even (+ve) or odd (-ve)
     red_tracker = 0 # This tracks the current likelihood of red (+ve) or black (-ve)
-    
-    # Starts with 2500
-    capital = 2500
-
-    # First bet
-    # Spin
-    # Result
-    # Process
-    # Analysis
-    # Select bets
-    # Place bets
-    # Spin
-
-    # Bets that can be placed
-    #   - Numbers
-    #   - Odds/Evens
-    #   - Black/Red
-    #   - Thirds
-    #   - Halves
 
     for i in range(1, 21):
-        #print("Enter a number to bet on:")
         choice = random.randint(1, 36)
         bet_clear()
         print(f"Placing bet on {choice}")
         place_bet(bet_cord[choice])
 
-        #
+        # bet if even/odd likely
         if even_tracker < -2:
             # bet even
             place_bet(bet_cord['even'])
@@ -190,7 +191,7 @@ def main():
             # bet odd
             place_bet(bet_cord['odd'])
         
-        #
+        # bet if red/black likely
         if red_tracker < -2:
             # bet red
             place_bet(bet_cord['red'])
